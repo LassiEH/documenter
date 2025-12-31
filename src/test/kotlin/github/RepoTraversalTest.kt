990f492b604cb510fc937b71dab04444b858613d
laelhaar <laelhaar@jyu.fi>
2025-12-31 15:10:02 +0200

    non existing file test added

--- src/test/kotlin/github/RepoTraversalTest.kt
+++ src/test/kotlin/github/RepoTraversalTest.kt
@@ -1,11 +1,9 @@
 package github
 
-import dataClasses.*
 import kotlin.test.Test
-import github.MockRepoNode
-import github.RepoHandler
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 class RepoTraversalTest {
     @Test
@@ -17,6 +15,15 @@ class RepoTraversalTest {
         assertNotNull(result)
         assertEquals("Main.kt", result.name)
         assertEquals("src/main/Main.kt", result.path)
+    }
+
+    @Test
+    fun `file does not exist`() {
+        val handler = RepoHandler()
+        val root = MockRepoNode.test()
+
+        val result = handler.findFile(root, "Nonexisting.kt")
+        assertNull(result)
     }
 
 }
